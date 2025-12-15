bb52bd66181f9eb8cfe24df903d09d595fbdd74a
ANH KHOA <anhkhoa512005@gmail.com>
2025-12-15 22:43:01 +0700

    add api and procedure

--- backend/routes/search.route.js
+++ backend/routes/search.route.js
@@ -4,13 +4,27 @@ import sql from "mssql";
 
 const router = express.Router();
 
+router.get('/tra-cuu-thu-cung-khach-hang', async (req, res) => {
+    const phone = req.query.phone;
+    try {
+        const result = await db.request()
+            .input('SDT', sql.Char(10), phone)
+            .execute('sp_TraCuuThuCung_SDT');
+
+        res.json(result.recordsets);
+    } catch (err) {
+        console.error("Error searching pets:", err);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
 // 10) Tra cứu hồ sơ bệnh án
 router.get('/tra-cuu-ho-so-benh-an', async (req, res) => {
-    const pet_name = req.query.name;
+    const pet_id = req.query.id;
     const num = req.query.num;
     try {
         const result = await db.request()
-            .input('TenThuCung', sql.NVarChar, `%${pet_name}%`)
+            .input('MaThuCung', sql.Char(15), pet_id)
             .input('SoLuongHoso', sql.Int, num)
             .execute('sp_TraCuuHosoBenhAn');
 
